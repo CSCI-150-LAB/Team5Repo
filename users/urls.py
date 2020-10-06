65dc2ac60d15dd51484aa0ffdf71b9004bed9762
psoliss <49050274+psoliss@users.noreply.github.com>
2020-10-05 21:02:32 -0700

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login_, name='login'),
-    path('signup/', views.signup_, name='signup')
+    path('signup/', views.signup_, name='signup'),
+    path('landing/', views.landingpage, name='landing'),
+    path('logout/', views.logout_, name='logout'),
 ]
