8a9f50ce5131cba998fc32bfa6c926966cebb5a0
psoliss <49050274+psoliss@users.noreply.github.com>
2020-10-20 16:21:19 -0700

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home name='home'),
+    path('', views.home, name='home'),
     path('users/', include('users.urls')),
     path('users/register/', include('users.urls')),
     path('users/login/', include('users.urls')),
